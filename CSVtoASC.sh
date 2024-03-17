c12bc66db36de58322d16e3f6b6d05ea3dce8770
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2024-03-17 13:33:30 +0000

    Update CSVtoASC.sh

--- CSVtoASC.sh
+++ CSVtoASC.sh
@@ -1,7 +1,7 @@
 mkdir ../ASC
 for i in *.csv; do
-	tmp=$( basename "$i" .csv );
+    tmp=$( basename "$i" .csv );
     sed 's/,/\n/3;P;D' $i > /tmp/$tmp.asc
-	sed 's/,/ /g' /tmp/$tmp.asc > ../ASC/$tmp.asc
-	rm /tmp/$tmp.asc
+    sed 's/,/ /g' /tmp/$tmp.asc > ../ASC/$tmp.asc
+    rm /tmp/$tmp.asc
 done;
